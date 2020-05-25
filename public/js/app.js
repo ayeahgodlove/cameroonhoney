b515bf52225a3f10ba83e08cefcca71d0b270c38
Ayeah Godlove <ayeahgodlove5@gmail.com>
2020-05-25 08:41:40 +0100

    more work on design handled sticky header and navbar change product show items contact section

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,74 @@
-// automatic active elements on nav-item
-const nav = document.querySelector('#navigation');
-nav.classList
+// Setip slick on banner 
+$(document).ready(function(){ 
+    // publications slick
+    $('.to_slick_it').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+            {
+            breakpoint: 1024,
+                // settings: {
+                //     slidesToShow: 3,
+                //     slidesToScroll: 3,
+                //     infinite: true,
+                //     dots: true
+                // },
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+
+    $('.slick-shopping-items').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1100,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 4,
+                        infinite: true,
+                        dots: true
+                    }
+                },
+            {
+            breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+            breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+            breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+})
